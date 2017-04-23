b9352013aa0474812b7c964a1ae2737d641a85b4
srp301 <656413927@qq.com>
2017-04-23 19:08:47 +0800

    2017-04-23

--- js/audit_list.js
+++ js/audit_list.js
@@ -1,9 +1,5 @@
 window.onload = function(){
-	var list = ["部门概况","法规制度","工作动态","工作动态1","工作动态2","工作动态3","文化建设","学习园地",
-				"学习园地1","学习园地2","学习园地3","工作清单","信息公开","信息公开1","信息公开2"];
-	var title_list = {"工作动态":["工作动态1","工作动态2","工作动态3"],
-						"学习园地":["学习园地1","学习园地2","学习园地3"],
-						"信息公开":["信息公开1","信息公开2"]};
+	var list = [{"0":"asdf"}];
 	var str1 = "<a href=\"audit_list.html?id=";
 	var str2 = "\">";
 	var str3 = "</a>";
@@ -26,14 +22,11 @@ window.onload = function(){
 	}
 	var args = getQueryString();
 	if(list[args.id]===undefined){
-		window.location.href="audit.html";
+		//window.location.href="audit.html";
 		return;
 	}
 	var current_location_str = str1+args.id+str2+list[args.id]+str3;
 	var current_location = document.getElementById("current_location");
 	var new_current_location = current_location.innerHTML;
 	current_location.innerHTML = new_current_location+current_location_str;
-	var title_id = list[args.id];
-	var content_text_title_ul = document.getElementById("content_text_title_ul");
-	
 };
